30a9809057afc8534b1748f01287f663739923f0
rmarush <rmarushkevych@gmail.com>
2023-09-30 14:45:45 +0300

    remove comments

--- HW3/HW3/Program.cs
+++ HW3/HW3/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //TaskOne();
-            //TaskTwo();
+            TaskOne();
+            TaskTwo();
             TaskThree();
         }
 
