75c541a4f4a55a9ef46f3a3d1e00c4a6b6dd289c
rmarush <rmarushkevych@gmail.com>
2023-10-03 10:21:03 +0300

    add hw4

---
+++ HW4/ConsoleApp1/Class/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Class
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthYear;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public DateTime BirthYear
+        {
+            set { birthYear = value; }
+            get { return birthYear; }
+        }
+        public Person() { }
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear; 
+        }
+        public int Age()
+        {
+            int age = DateTime.Now.Year - birthYear.Year;
+            age = DateTime.Now.Month < birthYear.Month || (DateTime.Now.Month == birthYear.Month && DateTime.Now.Day <  birthYear.Day ) ? --age : age;
+            return age;
+        }
+        public Person Input()
+        {
+            Console.Write("Input a name: ");
+            string name = Console.ReadLine();
+            Console.Write("Input a birthYear(format yyyy-mm-dd): ");
+            while(true)
+            {
+                string dateInput = Console.ReadLine();
+                if (DateTime.TryParse(dateInput, out DateTime birthDate))
+                {
+                    Person person = new Person(name, birthDate);
+                    return person;
+                }
+                else
+                {
+                    Console.WriteLine("Uncorrect input, try again!");
+                }
+            }
+        }
+        public void ChangeName(string name) 
+        {
+            this.name = name;
+        }
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public override string ToString()
+        {
+            return string.Format($"Name => {name} || Age => {Age()}");
+        }
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.name == person2.name;
+        }
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+    }
+}
+/*Define class Person. Class Person should consist of:
+              a) two private fields: name and birthYear (the birthday year).
+		(As a type for this field you may use DataTime type.)
+              b) two properties for access to these fields (can use only get)
+              c) default constructor and constructor with 2 parameters 
+              d) methods: - Age() - to calculate the age of person
+                   	  - Input() - to enter data about person from console
+                  	  - ChangeName() - to change the name of person
+                 	  - ToString() 
+                   	  - Output() - to output information about person (call ToString())
+                 	  - operator== (equal by name)
+In the method Main() create 6 objects of Person type and enter information about them. 
+Then calculate and output on the console name and Age of each person; 
+Change the name of persons, which Age is less then 16, to "Very Young".
+Output information about all persons on the console. 
+Find and output information about Persons with the same names (use ==).	
+*/
---
+++ HW4/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using ConsoleApp1.Class;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = CreateArray();
+            PrintInfo(person);
+            Console.Write("Input the age you would consider very young: ");
+            CheckAge(person, Int32.Parse(Console.ReadLine()));
+            PrintInfo(person);
+            Console.Write("Plese, input the name which you want to find: ");
+            FindSameName(person, Console.ReadLine());
+        }
+        public static void PrintInfo(Person[] person)
+        {
+            Console.WriteLine("Information about all persons: ");
+            foreach (Person pers in person)
+            {
+                Console.WriteLine(pers);
+            }
+        }
+        public static Person[] CreateArray()
+        {
+            Person[] person = new Person[6];
+            Person tempPers = new Person();
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine($"Information about Person №{i + 1}:");
+                person[i] = tempPers.Input();
+            }
+            return person;
+        } 
+        public static void CheckAge(Person[] person, int checkedAge)
+        {
+            for (int i = 0; i < person.Length; i++)
+            {
+                if (person[i].Age() < checkedAge) person[i].ChangeName("Very Young");
+            }
+        }
+        public static void FindSameName(Person[] person, string findName)
+        {
+            for (int i = 0; i < person.Length; i++)
+            {
+                for (int j = i + 1; j < person.Length; j++)
+                {
+                    if (person[i] == person[j]) Console.WriteLine($"{person[i]} and {person[j]} have the same name");
+                }
+            }
+        }
+    }
+}
+/*Define class Person. Class Person should consist of:
+              a) two private fields: name and birthYear (the birthday year).
+		(As a type for this field you may use DataTime type.)
+              b) two properties for access to these fields (can use only get)
+              c) default constructor and constructor with 2 parameters 
+              d) methods: - Age() - to calculate the age of person
+                   	  - Input() - to enter data about person from console
+                  	  - ChangeName() - to change the name of person
+                 	  - ToString() 
+                   	  - Output() - to output information about person (call ToString())
+                 	  - operator== (equal by name)
+In the method Main() create 6 objects of Person type and enter information about them. 
+Then calculate and output on the console name and Age of each person; 
+Change the name of persons, which Age is less then 16, to "Very Young".
+Output information about all persons on the console. 
+Find and output information about Persons with the same names (use ==).	
+*/
