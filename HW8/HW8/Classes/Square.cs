54564e30294cbf0ecb7f13778c7400c7e3821a3d
rmarush <rmarushkevych@gmail.com>
2023-10-17 10:00:53 +0300

    add hw8

---
+++ HW8/HW8/Classes/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8.Classes
+{
+    class Square : Shape
+    {
+        private double side;
+        public double Side { get { return side; } set {  side = value; } }
+        public Square(string name, double side) : base(name)
+        {
+            this.side = side;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Square => {Name}" +
+                $"\nArea => {CalculateArea()}" +
+                $"\nPerimetr => {CalculatePerimeter()}");
+        }
+        public override double CalculateArea()
+        {
+            return side * side;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 4 * side;
+        }
+    }
+}
+// Create classes Circle, Square derived from Shape with field radius (for Circle) and side (for Square).
